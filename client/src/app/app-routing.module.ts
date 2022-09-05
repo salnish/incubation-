1d66d24be6c4ae308ed48c5eecb60f8c9ded8eb1
salnish <103054536+salnish@users.noreply.github.com>
2022-09-05 12:15:57 +0530

    pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { LoginComponent } from './pages/admin/login/login.component';
+import { UserHomeComponent } from './pages/user/user-home/user-home.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { UserLoginComponent } from './pages/user/user-login/user-login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:UserLoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'userHome',
+    component:UserHomeComponent
+  },
+  {
+    path:'admin',
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
