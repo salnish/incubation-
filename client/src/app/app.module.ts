f88f9914dc21caca6947a0228604904575268413
salnish <103054536+salnish@users.noreply.github.com>
2022-09-05 14:16:29 +0530

    save

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/admin/login/login.component';
-import { RegisterComponent } from './pages/user/register/register.component';
-import { UserLoginComponent } from './pages/user/user-login/user-login.component';
-import { UserHomeComponent } from './pages/user/user-home/user-home.component';
-import { AdminHomeComponent } from './pages/admin/admin-home/admin-home.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    UserLoginComponent,
-    UserHomeComponent,
-    AdminHomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
